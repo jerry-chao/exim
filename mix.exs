e05dbabe2081007ad79579e0e10e159bce70582d
easemob <462283159@qq.com>
2025-06-20 23:58:58 +0800

    modify chat ui to full screen

--- mix.exs
+++ mix.exs
@@ -56,7 +56,8 @@ defmodule Exim.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:swoosh, "~> 1.19"}
+      {:swoosh, "~> 1.19"},
+      {:tidewave, "~> 0.1", only: :dev}
     ]
   end
 
